cfdfa0b657623c9a72ba0a6bef15b744bacaa7b5
Renan Carvalho <rcrvlh90@gmail.com>
2023-08-24 14:20:34 -0300

    Add files via upload

--- src/Components/CRUD/Crud.jsx
+++ src/Components/CRUD/Crud.jsx
@@ -4,7 +4,7 @@ const Crud = () => {
 	return (
 		<div className="bg-[url('/src/assets/hero-bg.jpg')] flex flex-col items-center p-16">
 			<div className="container w-auto shadow-2xl rounded-lg p-8 bg-gradient-to-b from-emerald-500 to-emerald-900">
-				<h2 className="text-2xl py-10 text-white">Cadastro Cliente</h2>
+				<h2 className="text-2xl pt-1 pb-10 text-white">Cadastro Cliente</h2>
 				<Create />
 			</div>
 		</div>
